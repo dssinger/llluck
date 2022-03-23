d6f57754d1aa954e1f9f8c1aa6248b97f4332d6a
David Singer <david@dsinger.com>
2022-03-22 22:25:49 -0700

    Use DingJY's pandas-based implementation
    I added code to generate the input for the message board; ref.py
    adds the framing for the message board postings.

--- llluck.py
+++ llluck.py
@@ -1,227 +1,114 @@
-#!/usr/bin/env python
-""" LearnedLeague Luck
+#!/usr/bin/env python3
+import pandas as pd
+import numpy  as np
+import sys
+from io import StringIO
+import re
 
-This is an implementation of SheahanJ's algorithm for computing
-"luck" in the [LearnedLeague](http://learnedleague.com), as described
-by his post at in the [LearnedLeague Forum](http://www.learnedleague.com/viewtopic.php?f=3&t=5250)
+def help():
+    print('Usage:', sys.argv[0], 'infile [season]', file=sys.stderr)
+    print(file=sys.stderr)
+    print('  infile: input file to read, typically with the season number as the first set of digits in the filename.', file=sys.stderr)
+    print('  season: the season number (required if the season number is not in the input filename)', file=sys.stderr)
+    print('', file=sys.stderr)
+    print('Produces three output files:', file=sys.stderr)
+    print('', file=sys.stderr)
+    print('  ll{season}.csv - a CSV including all the luck computations', file=sys.stderr)
+    print('  lucky{season}.bbcode - the luckiest 100 LLamas in BBCode format', file=sys.stderr)
+    print('  unlucky{season}.bbcode - the least lucky 100 LLamas in BBCode format', file=sys.stderr)
+    sys.exit(1)
 
-The program expects to receive a CSV of player records on STDIN, 
-in the format of the "all players" file from the LL site.
+infile = sys.argv[1]
+try:
+    season = sys.argv[2]
+except IndexError:
+    m = re.search(r'[0-9]+', infile)
+    if m:
+        season = m.group()
+    else:
+        help()
+        sys.exit(1)
+outfile = f'll{season}.csv'
+luckfile = f'lucky{season}.bbcode'
+unluckfile = f'unlucky{season}.bbcode'
 
-It generates three output files in the current directory:
 
-lucky.csv - a CSV with player names, records, Rundle, 
-   expected points, luck, and strengh-of-schedule as defined by SheahanJ
+df = pd.read_csv(infile, encoding='ISO-8859-1')
+df = df.rename(columns={'Rundle Rank':'Rank'})
 
-lucky.bbcode - a BBCode table showing the same information for the 
-   luckiest 100 LLamas (all output rounded to 2 decimal places)
+count_stats = ['Rundle', 'Player']
+sum_stats   = ['Rundle', 'FL', 'FW', 'TCA', 'Q']
 
-unlucky.bbcode - a BBCode table showing the same information for the
-   unluckiest 100 LLamas 
-"""
+df['Matches'] = df['Wins'] + df['Losses'] + df['Ties']
+df['Played']  = df['Matches'] - df['FL']
+df['FPct']    = df['FL'] / df['Matches']
+df['Q']       = 6 * df['Played']
+df['MPA']     = df['TMP'] - df['MPD']
 
-import csv, os, sys, math
+count = df[count_stats].groupby('Rundle', as_index=False).count()
+sum   = df[sum_stats].groupby('Rundle', as_index=False).sum()
 
-def normalize(s):
-    s = s.replace(' ','')
-    if not s[0].isalpha():
-        s = '_' + s
-    abbrevs = {'Wins': 'W', 'Losses': 'L', 'Ties': 'T'}
-    if s in abbrevs:
-        s = abbrevs[s]
-    return s
-    
-def mean(l):
-    return sum(l) / len(l)
-    
-class Rundle(object):
-    rundles = {}
-    
-    @classmethod
-    def get(self, name):
-        if name not in self.rundles:
-            self.rundles[name] = self(name)
-        return self.rundles[name]
-            
-    def __init__(self, name):
-        self.name = name
-        self.players = {}
-        self.tFL = 0
-        self.tFW = 0
-        self.tTCA = 0
-        self.tQ = 0
-        self.pcount = 0
-        
-    def addPlayer(self, player):
-        self.players[player.Player] = player
-        self.tFL += player.FL
-        self.tFW += player.FW
-        self.tTCA += player.TCA
-        self.tQ += player.Q
-        self.pcount += 1
-            
-                    
-        
-        
-    
-    
-class Player(object):
-    players = {}
-    @classmethod
-    def get(self, name):
-        return self.players[name]
-    
-    def __init__(self, dict):
-        # Clean up the data from the spreadsheet and put it into the object
-        for key in dict: 
-            newkey = normalize(key)
-            value = dict[key]
-            try:
-                value = int(value)
-            except ValueError:
-                try:
-                    value = float(value)
-                except ValueError:
-                    pass
-                
-            
-            self.__dict__[newkey] = value
-            
-        # Compute additional stats
-        self.played = self.W + self.L + self.T
-        self.forfeitRate = self.FL / self.played
-        self.Q = 6 * (self.played - self.FL)
-        self.MPA = self.TMP - self.MPD
-        
-        # Link into the rundle
-        self.realRundle = Rundle.get(self.Rundle)
-        self.realRundle.addPlayer(self)
-        
-        # And save
-        self.players[self.Player] = self
-        
-    def computeStats(self):
-        rr = self.realRundle
-        oFL = rr.tFL - self.FL  # Games forfeited by others
-        aoFL = oFL / (rr.pcount - 1)  # Average games forfeited by others
-        self.oFR = (1.0 * oFL) / (self.played * (rr.pcount - 1))  # Other forfeit rate
-        self.xFW = self.oFR * (self.played - self.FL) # Expected Forfeit Wins
-       
+count = count.rename(columns={'Player':'Players'})
+sum   = sum.rename(columns={'FL':'rFL', 'FW':'rFW', 'TCA':'rTCA', 'Q':'rQ'})
 
-        self.rQPCT = 1.0 * (rr.tTCA - self.TCA)/(rr.tQ - self.Q)
-        self.xCAA = self.rQPCT * 6 * (25-self.xFW-self.FL)
-        self.xMPA = self.PCAA * self.xCAA
-        self.SOS = (self.CAA / (6.0 * (self.played - self.FW))) / self.rQPCT
-        
+df = df.merge(count)
+df = df.merge(sum)
 
-        try:
-            self.xTMP = self.TMP*(25.0-self.xFW-self.FL)/(25.0-self.FL)
-        except ZeroDivisionError:
-            self.xTMP = 0
+df['oFL']  = df['rFL'] - df['FL']
+df['aoFL'] = df['oFL'] / (df['Players'] - 1)
+df['oFR']  = df['aoFL'] / df['Matches']
+df['xFW']  = df['oFR'] * df['Played']
 
-        try:
-            self.pwp = 1/(1+(self.xMPA/self.xTMP)**1.93)
-        except ZeroDivisionError:
-            self.pwp = 0
+df['rQPct'] = (df['rTCA'] - df['TCA']) / (df['rQ'] - df['Q'])
+df['xCAA']  = df['rQPct'] * 6 * (df['Played'] - df['xFW'])
+df['xMPA']  = df['PCAA'] * df['xCAA']
+df['SOS']   = df['CAA'] / (6 * (df['Matches']-df['FW']) * df['rQPct'])
+df['xTMP']  = df['TMP'] * (df['Played'] - df['xFW']) / df['Played']
+df['PWP']   = 1 / (1 + (df['xMPA'] / df['xTMP'])**1.93)
+df = df.replace([np.inf, -np.inf, np.nan], 0)
 
-        self.xPts = 2*self.pwp*(25-self.xFW-self.FL) + (2*self.xFW) - self.FL
-            
-        self.luck = (self.Pts- self.xPts) / 2
-        
-        
-        
-    def out(self, stats):
-        ans = [self.__dict__[normalize(t)] for t in stats]
-        return ans
-    
-def output(filename, data, stats, fmts):
-    # Unfortunately, the BBCode implementation on LearnedLeague doesn't properly
-    # handle tables (it puts a huge amount of whitespace above the table), so we
-    # have to use pre-formatted text instead.  We replace spaces with underscores
-    # for purposes of clarity.
-    outfile = open(filename, 'w')
-    outfile.write('[code]\n')
-    
-    res = []
-    outline = ['Rank']
-    # We need to figure out how wide to make each column.
-    # Start by leaving enough room for the item names
-    widths = {}     
-    for item in stats:
-        outline.append(item)
-        widths[item] = len(item)
-    res.append(outline)
-    
-    
-    
-    linenum = 0
-    for line in data:
-        linenum += 1
-        outline = ['%4d' % linenum]
-        for item in stats:
-            if 's' in fmts[item]:
-                value = fmts[item] % line.__dict__[item].strip()
-            else:
-                value = fmts[item] % line.__dict__[item]
-            outline.append(value)
-            widths[item] = max(widths[item],len(value))
-        res.append(outline)
-        
-    # Now, we have all of the results in outline, and widths tells us how wide each
-    #   column must be, so we can output the results
-    
-    fmtline  = '%4s | ' + ' | '.join([('%%%ds' if 's' not in fmts[item] else '%%-%ds') % widths[item] for item in stats])
-    for line in res:
-        outfile.write(fmtline % tuple(line))
-        outfile.write('\n')
-        
-    outfile.write('[/code]\n')
-        
-   
-   
-    outfile.close()
-    
- 
+df['xPts'] = (2*df['PWP']*(df['Played']-df['xFW'])) + (2*df['xFW']) - df['FL']
+df['Luck'] = (df['Pts'] - df['xPts']) / 2
 
-if __name__ == '__main__': 
-    # Get the data
-    reader = csv.DictReader(sys.stdin)
-    for row in reader:
-        Player(row)
-    
-    # Now we can compute all statistics
-    for p in Player.players.values():
-        p.computeStats()
-        
-    # Sort the players from luckiest to least lucky
-    sortedlist = sorted(Player.players.values(), key=lambda x:0-x.luck)
+df['xRank'] = df.groupby('Rundle')['xPts'].rank('dense', ascending=False).astype(int)
+df['LuckPctile'] = 100 * (df['Luck'].rank() - 1) / len(df)
 
-    # Establish columns for output
-    stats = [
-        'Player', 'W', 'L', 'T', 'Pts', 'MPD', 'Rundle', 'xPts', 'luck', 'SOS'
-    ]
-    fmts = {'Player': '%s', 'W': '%2d', 'L': '%2d', 'T':'%2d', 'Pts':'%3d',
-            'MPD': '%4d', 'Rundle': '%s', 'xPts': '%7.2f', 'luck': '%7.2f', 'SOS' : '%5.2f'}
+df = df.sort_values('Luck', ascending=False)
 
-    # The spreadsheet gets more information
-    ssstats = ['Player', 'Rundle', 'Wins', 'Losses', 'Ties', 'Pts', 'xPts', 'MPD', 'FL', 'FW', 'xFW', 'pwp', 'QPct', 'rQPCT', 'PCAA', 'CAA', 'xCAA', 'MPA', 'xMPA', 'TMP', 'xTMP', 'luck', 'SOS']
+columns = ['Player', 'Rundle', 'Branch', 'Wins', 'Losses', 'Ties', 'Pts',
+           'xPts', 'Rank', 'xRank', 'MPD', 'FL', 'FW', 'xFW', 'PWP', 'QPct',
+           'rQPct', 'PCAA', 'CAA', 'xCAA', 'MPA', 'xMPA', 'TMP', 'xTMP', 'Luck',
+           'SOS', 'LuckPctile']
 
-        
-    # Generate the total list
-    writer = csv.writer(open('lucky.csv','wb'))
-    writer.writerow(ssstats)
-    
-    for p in sortedlist:
-        writer.writerow(p.out(ssstats))
-    
-    # And now, let's generate the luckiest and unluckiest 100 LLamas
-    output('lucky.bbcode', sortedlist[0:100], stats, fmts)
-    
-    # For unlucky LLamas, we need to ignore Pavanos.
-    index = -1
+df[columns].to_csv(outfile, index=False)
 
-    while sortedlist[index].FL == 25:
-        index -= 1
-    
-    output('unlucky.bbcode', sortedlist[index:index-100:-1], stats, fmts)
-    
+tblcols = ['Player', 'Rundle', 'Wins', 'Losses', 'Ties', 'Pts', 'MPD', 'xPts', 'Luck', 'Rank', 'xRank', 'SOS']
+
+
+# Now build the tables for the discussion board.  Each table has to be exactly ONE line
+#   to avoid problems with the table formatter on the site.
+def maketable(frame):
+    s = StringIO()
+    zeta = 'ζ'
+    frame.to_csv(s, columns=tblcols, index=False, sep=zeta, float_format=lambda x:f'{x:.2f}')
+
+    lines = s.getvalue().split('\n')
+
+    out = ['[table]']
+    out.append('[tr][th]')
+    out.append(lines[0].replace(zeta, '[/th][th]'))
+    out.append('[/th][/tr]')
+    for line in lines[1:]:
+        out.append('[tr][td]')
+        out.append(line.replace(zeta, '[/td][td]'))
+        out.append('[/td][/tr]')
+    out.append('[/table]')
+    return ''.join(out)
+
+with open(luckfile, 'w') as f:
+    f.write(maketable(df[0:100][tblcols]))
+    f.write('\n')
+
+with open(unluckfile, 'w') as f:
+    f.write(maketable(df[-100:][tblcols]))
+    f.write('\n')
--- ref.py
+++ ref.py
@@ -44,29 +44,31 @@ if 'dropbox' not in url:
 
 # OK, now we know what to put into the prefatory text
 
-infile = open('lucky.bbcode')
-outfile = open('lucky.txt', 'w')
-outfile.write('I implemented SheahanJ\'s algorithm from the first posts in this forum'
-' as a Python program (source code at'
-f' [url]https://github.com/dssinger/llluck[/url]) and ran it against the LL{season}'
-f' season ([url]https://www.learnedleague.com/lgwide.php?{season}[/url]).')
-outfile.write('\n\n')
+infile = open(f'lucky{season}.bbcode', 'r')
+outfile = open(f'lucky{season}.txt', 'w')
+outfile.write('')
+outfile.write('I implemented SheahanJ\'s algorithm from the first posts in this forum in Python; DengJY improved'
+              ' the code by using the Pandas package and added the computation of "xRank" (expected rank).  Each'
+              ' season, I run the code and post'
+              ' the top 100 and bottom 100 "lucky" LLamas to the message board.'
+              '\n\n'
+              f'You can find the full results for season {season} on Dropbox at [url]{url}[/url].  The results for'
+              ' seasons beginning with season 70 are in this'
+              ' [url=https://www.dropbox.com/sh/79w2o3m2wv5txgm/AADGWDoBwQDr7HDveVMLrWPBa?dl=0]Dropbox folder[/url].'
+              '\n\n'
+              'The code is available on GitHub at [url]https://github.com/dssinger/llluck[/url].)'
+              '\n\n')
 
-outfile.write('The full results for the whole league are available as a CSV on Dropbox at'
-f' [url]{url}[/url] and results for LL seasons beginning with season 70,'
-' along with the code, are in this'
-' [url=https://www.dropbox.com/sh/79w2o3m2wv5txgm/AADGWDoBwQDr7HDveVMLrWPBa?dl=0]Dropbox folder[/url].')
-outfile.write('\n\n')
 outfile.write('The top 100 "lucky" LLamas are listed below, and the bottom 100 will be in the next posting.\n\n')
 
 
-outfile.write(dofile(infile))
+outfile.write(infile.read())
 outfile.close()
 
-infile = open('unlucky.bbcode')
-outfile = open('unlucky.txt', 'w')
+infile = open(f'unlucky{season}.bbcode', 'r')
+outfile = open(f'unlucky{season}.txt', 'w')
 outfile.write(f'Here are the 100 least lucky LLamas for Season {season}.\n\n')
-outfile.write(dofile(infile))
+outfile.write(infile.read())
 outfile.close()
 
 
